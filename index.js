36ea041076e5483fae4c7658255c38e325332f79
Aleksey Sokolov <alexis871aa@yandex.ru>
2024-06-20 17:53:54 +0300

    modified index.js - module http

--- index.js
+++ index.js
@@ -1,51 +1,16 @@
-const yargs = require('yargs')  // данный пакет нам поможет в чтении параметров и в формировании документации
-const pkg = require('./package.json') // подключаем package.json как JS объект
+// стандартный модуль для создания веб-интерфейса http
+const http = require('http')
+const chalk = require('chalk')
 
-const {addNote, getNotes, printNotes, removeNote} = require('./notes.controller') // подключаем модуль notes.controller
+const PORT = 3005
 
-// метод для актуализации версии программы (появляется по команде node index --version)
-yargs.version(pkg.version)
+const server = http.createServer((req, res) => {
+	console.log('Request object:', req.method) // смотрим метод, с которым мы делали запрос
+	console.log('Request object:', req.url) // смотрим url запроса
 
-// регистрируем команду для добавления заметки в лист заметок
-yargs.command({   // WebStorm показывает, что он не знает такую команду, лечиться npm i @types/yargs
-   command: 'add',
-   describe: 'Add new note to list',
-   builder: {
-      title: {
-         type: 'string',
-         describe: 'Note title',
-         demandOption: true,  // это обязательный параметр для работы данной команды
-      }
-   },
-   async handler({ title }) { // деструктуризируем options считывает параметры консоли
-      await addNote(title)
-   }
+	res.end('Hello from server!')
 })
 
-// регистрируем команду для вывода всех заметок в листе заметок
-yargs.command({
-   command: 'list',
-   describe: 'Print all notes',
-   async handler() {
-      await printNotes()
-   }
+server.listen(PORT, () => {
+	console.log(chalk.green(`Server has been started on port ${PORT}...`))
 })
-
-// регистрируем команду для удаления заметок по ID
-yargs.command({
-   command: 'remove',
-   describe: 'Remove note by id',
-   builder: {
-      id: {
-         type: 'string',
-         describe: 'Note ID',
-         demandOption: true,  // это обязательный параметр для работы данной команды
-      }
-   },
-   async handler({ id }) {
-      await removeNote(id)
-   }
-})
-
-// инициализируем команды
-yargs.parse()
